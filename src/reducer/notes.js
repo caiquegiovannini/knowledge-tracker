8c0e433f39c4a9cbdfb8c17bc4d9922c7ef3295a
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2026-02-12 22:14:46 -0300

    Create `reducer`

---
+++ src/reducer/notes.js
@@ -0,0 +1,27 @@
+import { ADD_NOTE } from "../action-type/notes.js";
+/** @import { Action } from '../types/action.js' */
+
+const initialState = { notes: [] };
+
+/** 
+ * @param {Action} action 
+ */
+function notesReducer(state = initialState, action) {
+    switch (action.type) {
+        case ADD_NOTE:
+            return {
+                ...state,
+                notes: [
+                    ...state.notes,
+                    action.payload
+                ]
+            };
+    
+        default:
+            break;
+    }
+}
+
+export {
+    notesReducer
+}
