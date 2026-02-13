5f70afdb17c38c2c0e75fe9d7400fa63f2d51bb9
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2026-02-12 22:14:10 -0300

    Create `store` with private state and public interface with `getState`, `dispatch` and `subscribe` functions

---
+++ src/store/notes.js
@@ -0,0 +1,41 @@
+/** @import { Action } from '../types/action.js' */
+
+import { notesReducer } from '../reducer/notes.js';
+
+function createStore(initialState = { notes: [] }) {
+    let state = initialState;
+    const listeners = new Set([]);
+
+    function getState() {
+        return state;
+    }
+
+    function setState(newState) {
+        state = newState;
+    }
+
+    /**@param {Action} action */
+    function dispatch(action) {
+        const updatedState = notesReducer(state, action);
+        setState(updatedState);
+
+        // para iterar sobre uma cópia de listeners e não muta-lo durante o loop
+        const currentListeners = [...listeners];
+        currentListeners.forEach(listener => listener());
+    }
+    
+    /**@param {() => void} listener */
+    function subscribe(listener) {
+        listeners.add(listener);
+
+        return () => listeners.delete(listener)
+    }
+
+    return {
+        getState,
+        dispatch,
+        subscribe
+    }
+}
+
+export { createStore }
