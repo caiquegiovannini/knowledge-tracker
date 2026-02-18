efd07e4eaeb3a0dcba40b5cb6c4a05d4b695eb0b
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2026-02-17 23:49:36 -0300

    Create Note model type

---
+++ src/modules/notes/domain/note.types.js
@@ -0,0 +1,10 @@
+/**
+ * @typedef {Object} Note
+ * @property {string} id
+ * @property {string} title
+ * @property {string} content
+ * @property {Note|null} revision
+ * @property {Date} createdAt 
+ */
+
+export {};
