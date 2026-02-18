ed7ee7ad3c18dd7f600fc90700c8b872f02913bb
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2026-02-17 23:50:59 -0300

    Add jsdoc typing to `createStore`

--- src/app/store/create-store.js
+++ src/app/store/create-store.js
@@ -1,28 +1,34 @@
-/** @import { Action } from '../../types/action.js' */
+/** @import { Action } from '@/types/action.js' */
 
+/**
+ * @template S, P
+ * @param {(state: S, action: Action<P>) => S} reducer 
+ * @param {S} initialState 
+ * @returns {{getState: () => S, dispatch: (action: Action<P>) => void, subscribe: (listener: () => void) => () => void}}
+ */
 function createStore(reducer, initialState) {
     let state = initialState;
-    const listeners = new Set([]);
+    const listeners = new Set();
 
     function getState() {
         return state;
     }
 
+    /** @param {S} newState */
     function setState(newState) {
         state = newState;
     }
 
-    /**@param {Action} action */
+    /**@param {Action<P>} action */
     function dispatch(action) {
         const updatedState = reducer(state, action);
         setState(updatedState);
 
         // para iterar sobre uma cópia de listeners e não muta-lo durante o loop
-        const currentListeners = [...listeners];
+        const currentListeners = Array.from(listeners);
         currentListeners.forEach(listener => listener());
     }
     
-    /**@param {() => void} listener */
     function subscribe(listener) {
         listeners.add(listener);
 
